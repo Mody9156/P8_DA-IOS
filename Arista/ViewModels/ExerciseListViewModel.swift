183c0ee8993b12b5a7a289566a572091fe0d4fc2
Modibo KEITA <modykeita2@gmail.com>
2024-08-01 17:43:14 +0200

    update function fetchExercises and delete struct FakeExercise

--- Arista/ViewModels/ExerciseListViewModel.swift
+++ Arista/ViewModels/ExerciseListViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 import CoreData
 
 class ExerciseListViewModel: ObservableObject {
-    @Published var exercises = [FakeExercise]()
+    @Published var exercises = [Exercise]()
 
     var viewContext: NSManagedObjectContext
 
@@ -21,15 +21,22 @@ class ExerciseListViewModel: ObservableObject {
 
     private func fetchExercises() {
         // TODO: fetch data in CoreData and replace dumb value below with appropriate information
-        exercises = [FakeExercise(), FakeExercise(), FakeExercise()]
+//        exercises = [FakeExercise(), FakeExercise(), FakeExercise()]
+        do{
+            let data = ExerciseRepository(viewContext: viewContext)
+            exercises = try data.getExercise()
+        }catch{
+            
+        }
+        
     }
 }
-
-struct FakeExercise: Identifiable {
-    var id = UUID()
-    
-    var category: String = "Football"
-    var duration: Int = 120
-    var intensity: Int = 8
-    var date: Date = Date()
-}
+//
+//struct FakeExercise: Identifiable {
+//    var id = UUID()
+//
+//    var category: String = "Football"
+//    var duration: Int = 120
+//    var intensity: Int = 8
+//    var date: Date = Date()
+//}
