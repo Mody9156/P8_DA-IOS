2b2ca2ecf621f75d9e1b865762b11b0e52d4eecc
Modibo KEITA <modykeita2@gmail.com>
2024-08-01 14:52:09 +0200

    add new struct for NSManagedObjectContext

---
+++ Arista/Models/SleepRepository.swift
@@ -0,0 +1,28 @@
+//
+//  SleepRepository.swift
+//  Arista
+//
+//  Created by KEITA on 01/08/2024.
+//
+
+import Foundation
+import CoreData
+
+struct SleepRepository {
+    
+    // MARK: - Properties
+    
+    let viewContext : NSManagedObjectContext
+    
+    init(viewContext: NSManagedObjectContext = PersistenceController.shared.container.viewContext) {
+        self.viewContext = viewContext
+    }
+    
+    // MARK: - Public
+    
+    func getSleepSessions() throws -> [Sleep] {
+        let request = Sleep.fetchRequest()
+        [NSSortDescriptor(SortDescriptor<Sleep>(\.startDate,order: .reverse))]
+        return try viewContext.fetch(request)
+    }
+}
---
+++ Arista/Models/UserRepository.swift
@@ -0,0 +1,31 @@
+//
+//  UserRepository.swift
+//  Arista
+//
+//  Created by KEITA on 01/08/2024.
+//
+
+import Foundation
+import CoreData
+
+struct UserRepository {
+    
+    // MARK: - Properties
+    
+    let viewContext : NSManagedObjectContext
+    
+    init(viewContext: NSManagedObjectContext = PersistenceController.shared.container.viewContext
+    ) {
+        self.viewContext = viewContext
+    }
+    
+    // MARK: - Public
+    
+    func getUser() throws -> User? {
+        let request = User.fetchRequest()
+        request.fetchLimit = 1
+        return try viewContext.fetch(request).first
+                 
+    }
+    
+}
