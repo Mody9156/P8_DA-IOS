8cfdc91fb04dbf30cbd18a1e877ffcc98b337e16
Modibo KEITA <modykeita2@gmail.com>
2024-08-07 18:49:14 +0200

    created new fichier teste for addExercie

---
+++ AristaTests/ViewModel/AddExerciseViewModelTests.swift
@@ -0,0 +1,84 @@
+//
+//  AddExerciseViewModelTests.swift
+//  AristaTests
+//
+//  Created by KEITA on 07/08/2024.
+//
+
+import XCTest
+@testable import Arista
+import Combine
+import CoreData
+
+final class AddExerciseViewModelTests: XCTestCase {
+    var cancellable = Set<AnyCancellable>()
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    private func emptyEntities(context: NSManagedObjectContext) {
+
+    let fetchRequest = Exercise.fetchRequest()
+
+    let objects = try! context.fetch(fetchRequest)
+
+     
+
+    for exercice in objects {
+
+    context.delete(exercice)
+
+    }
+
+    try! context.save()
+
+    }
+
+    private func addExercice(context: NSManagedObjectContext, category: String, duration: Int, intensity: Int, startDate: Date, userFirstName: String, userLastName: String) {
+
+    let newUser = User(context: context)
+
+    newUser.firstName = userFirstName
+
+    newUser.lastName = userLastName
+
+    try! context.save()
+
+     
+
+    let newExercise = Exercise(context: context)
+
+    newExercise.category = category
+
+    newExercise.duration = Int64(duration)
+
+    newExercise.intensity = Int64(intensity)
+
+    newExercise.startDate = startDate
+
+    newExercise.user = newUser
+
+    try! context.save()
+
+    }
+}
