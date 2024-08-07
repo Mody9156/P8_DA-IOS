0a827e55fbdc9f860a0cc579f8ddac35a1a7b657
Modibo KEITA <modykeita2@gmail.com>
2024-08-07 12:10:13 +0200

    created new fichier tests

---
+++ AristaTests/Model/UserRepositoryTests.swift
@@ -0,0 +1,33 @@
+//
+//  UserRepositoryTests.swift
+//  AristaTests
+//
+//  Created by KEITA on 07/08/2024.
+//
+
+import XCTest
+@testable import Arista
+import CoreData
+
+final class UserRepositoryTests: XCTestCase {
+
+//    override func setUp() throws {
+//        super.setUp()
+//    }
+//
+//    override func tearDown() throws {
+//        super.tearDown()
+//    }
+
+    private func emptyEntities(context:NSManagedObjectContext){
+        
+    }
+    func testExample() throws {
+      
+    }
+
+    func testPerformanceExample() throws {
+        
+    }
+
+}
