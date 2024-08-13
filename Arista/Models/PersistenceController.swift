50f6fa6c18589d00e45d61e73c44c8daca17d3b7
Modibo KEITA <modykeita2@gmail.com>
2024-08-13 20:22:30 +0200

    update newbackgroundContext

--- Arista/Models/PersistenceController.swift
+++ Arista/Models/PersistenceController.swift
@@ -4,10 +4,9 @@
 //
 //  Created by Vincent Saluzzo on 08/12/2023.
 //
-
 import CoreData
 
-struct PersistenceController {
+class PersistenceController {
     
     // MARK: - Singleton
     
@@ -19,8 +18,6 @@ struct PersistenceController {
         do {
             try viewContext.save()
         } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             let nsError = error as NSError
             fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
@@ -28,37 +25,34 @@ struct PersistenceController {
     }()
 
     let container: NSPersistentContainer
+    let backgroundContext : NSManagedObjectContext
 
     // MARK: - Init
     
     init(inMemory: Bool = false) {
-        
         container = NSPersistentContainer(name: "Arista")
+        backgroundContext = container.newBackgroundContext()
         
         if inMemory {
             try! DefaultData(viewContext: container.viewContext).apply()
         }
         
-        
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
-        })
+        }
         
-        container.viewContext.automaticallyMergesChangesFromParent = true //merge
-            try! DefaultData(viewContext: container.viewContext).apply()
-
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        
+        // Utilisation de backgroundContext pour charger des données par défaut
+        backgroundContext.perform {
+            do {
+                try DefaultData(viewContext: self.backgroundContext).apply()
+                try self.backgroundContext.save()
+            } catch {
+                fatalError("Failed to apply default data: \(error.localizedDescription)")
+            }
+        }
     }
 }
