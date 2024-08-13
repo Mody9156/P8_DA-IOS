0c80ec518a1f0ce6600a3f9871497f0656e6b41d
Modibo KEITA <modykeita2@gmail.com>
2024-08-13 13:08:54 +0200

    initialise new slider

--- Arista/Views/AddSleepView.swift
+++ Arista/Views/AddSleepView.swift
@@ -10,29 +10,36 @@ import SwiftUI
 struct AddSleepView: View {
     @State private var slider = 0.0
     @ObservedObject var viewModel: SleepHistoryViewModel
-   
+    @Environment(\.presentationMode) var presentationMode
+
     var body: some View {
         NavigationStack {
             VStack {
                 Form{
                     
                         Section{
-                            Text("Durée : \(Int(slider)) minute(s)")
+                            Text("Durée : \(Int(slider)) heure(s)")
                             Slider(
                                 value: $slider,
                                 in: 0...120){} minimumValueLabel : {
                                     Text("0")
                                 } maximumValueLabel: {
-                                    Text("120")
+                                    Text("20")
                                 }
 //                                .onChange(of: slider) { newValue in
 //                                    viewModel.sleepSessions.quality = Int64(Int(slider))
 //                                }
                         }
+                    Spacer()
+                    Button("Ajouter l'exercice") {
+//                        if viewModel.reload {
+//                            presentationMode.wrappedValue.dismiss()
+//                        }
+                    }.buttonStyle(.borderedProminent)
 
                    
                }
-            }
+            } .navigationTitle("Nouvel element ...")
         }
     }
 }
