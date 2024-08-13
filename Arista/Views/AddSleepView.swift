cb0a32ab133cf338e2bb9c4bcc1fc92fd654705a
Modibo KEITA <modykeita2@gmail.com>
2024-08-13 14:00:24 +0200

    add datepicker

--- Arista/Views/AddSleepView.swift
+++ Arista/Views/AddSleepView.swift
@@ -11,20 +11,26 @@ struct AddSleepView: View {
     @State private var slider = 0.0
     @ObservedObject var viewModel: SleepHistoryViewModel
     @Environment(\.presentationMode) var presentationMode
+    @State var date : Date = Date.distantFuture
 
     var body: some View {
         NavigationStack {
             VStack {
                 Form{
                     
+                    DatePicker("Heure de démarrage : ", selection: $date,displayedComponents: .hourAndMinute)
+//                        .onChange(of: date) { newDate in
+//                            viewModel.startTime = newDate
+//                        }
+                    
                         Section{
                             Text("Durée : \(Int(slider)) heure(s)")
                             Slider(
                                 value: $slider,
-                                in: 0...120){} minimumValueLabel : {
+                                in: 0...24){} minimumValueLabel : {
                                     Text("0")
                                 } maximumValueLabel: {
-                                    Text("20")
+                                    Text("24")
                                 }
 //                                .onChange(of: slider) { newValue in
 //                                    viewModel.sleepSessions.quality = Int64(Int(slider))
